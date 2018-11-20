60d435860a7643ee0610e2c9b563988d325adc4d
WonJo94 <iamdnjswh@gmail.com>
2018-11-20 17:15:32 +0900

    Add:button interface

--- main.py
+++ main.py
@@ -5,25 +5,28 @@ from unicornled import UnicornLED
 from vibratesw import vibrateSW
 from bluetoothrfcomm import BluetoothRFCOMM
 from filemgr import FileManager
-from gyroscope import Gyroscope
-
+# from gyroscope import Gyroscope
+from Button_Interface import Button
 # -------------------- DEFINE LED ------------------ #
-
+## sadfasdfastest asdfsdf asdfasdfssg
 
 
 
 def main():
     filemanager = FileManager()
     # sw = vibrateSW(23)
+    # asdfsdfasdfasdfsd
     bluetooth = BluetoothRFCOMM()
-    #gyroSensor = Gyroscope()
+    # gyroSensor = Gyroscope()
     led = UnicornLED(filemanager.readState(), filemanager.saveLEDState)
+    start = Button()
 
     try:
         # sw.run(led.setEmergency, bluetooth.sendMsg)
         led.run()
         bluetooth.run(led.setAttribute, led.getLEDInfo)
-        #gyroSensor.run(led.inturrptLED, bluetooth.sendMsg)
+        # gyroSensor.run(led.inturrptLED, bluetooth.sendMsg)
+        start.run()
 
     except KeyboardInterrupt:
         print("main KeyboardInterrupt")
